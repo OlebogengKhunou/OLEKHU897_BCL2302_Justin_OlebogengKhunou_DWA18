b84158c3a99d971b64a195d93059097bb33dca07
OlebogengKhunou <125869902+OlebogengKhunou@users.noreply.github.com>
2023-08-11 08:34:11 +0200

    Added loading phase and sorting functionality of favourites

--- OLEKHU897_BCL2302_Justin_OlebogengKhunou_DWA18/src/components/Favourites.jsx
+++ OLEKHU897_BCL2302_Justin_OlebogengKhunou_DWA18/src/components/Favourites.jsx
@@ -1,16 +1,19 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useSyncExternalStore } from "react";
 import { supabase } from "./SignIn";
 
 export default function Favorites(props) {
     const [favouriteData, setFavouriteData] = useState([]);
-    const [state,setState] = useState('loading')
+    const [state, setState] = useState('loading')
+    const [sortState, setSortState] = useState('originData')
+    const [sortPhase, setSortPhase] = useState([])
 
     useEffect(() => {
         const fetchData = async () => {
             try {
                 const { data, error } = await supabase
                     .from('Favourites')
-                    .select('*');
+                    .select('*')
+                    .eq('Email', props.email)
                 if (error) {
                     console.error('Error fetching data:', error.message);
                 } else {
@@ -23,45 +26,85 @@ export default function Favorites(props) {
         };
 
         fetchData(); // Call the function to fetch data when the component mounts
-    }, []);
+    }, [favouriteData]);
 
-    const handleDelete = async (title) => {
-        const {data,error} = await supabase
-        .from('Favourites')
-        .delete()
-        .eq('EpisodeTitle', title)
- 
-     if(error){
-         console.log(error)
-     }
-     if (data){
-         setFavouriteData(data)
-     }
-    }
     
-    const favouriteElements = favouriteData.map(item => {
+    const handleDelete = async (title) => {
+        setSortState('originData')
+        const { data, error } = await supabase
+            .from('Favourites')
+            .delete()
+            .eq('EpisodeTitle', title)
+            .eq('Email', props.email)
+
+        if (error) {
+            console.log(error)
+        }
+        if (data) {
+
+        }
+    }
+
+
+
+
+    function sortByAscending() {
+        setSortState('sortByAscending')
+        setSortPhase(favouriteData.sort((a, b) => b.Show.localeCompare(a.Show)))
+    }
+    function sortByDescending() {
+        setSortState('sortByDescending')
+        setSortPhase(favouriteData.sort((a, b) => a.Show.localeCompare(b.Show)))
+    }
+    function sortByLatest() {
+        setSortState('sortByLatest')
+        setSortPhase(favouriteData.sort((a, b) => new Date(a.TimeStamp) - new Date(b.TimeStamp)))
+    }
+    function sortByOldest() {
+        setSortState('sortByOldest')
+        setSortPhase(favouriteData.sort((a, b) => new Date(b.TimeStamp) - new Date(a.TimeStamp)))
+    }
+
+
+    const favouriteElements = (sortState === 'originData' ? favouriteData : sortPhase).map(item => {
+
         const AddedDate = new Date(item.TimeStamp)
         return (
             <div
                 key={item.id}
                 className="episodes"
-            >   
-                <button  id={item.EpisodeFile} title={item.EpisodeTitle} onClick={props.HandleAudioPlay}>Play</button>
-                <p>Episode: {item.EpisodeNumber}</p>
-                <p>Title: {item.EpisodeTitle}</p>
+
+            >
                 <p>Show: {item.Show}</p>
                 <p>Seasons: {item.Season}</p>
-                <p>Added: {AddedDate.toLocaleDateString('en-GB')}</p>
-                <button onClick={()=>handleDelete(item.EpisodeTitle)}>DELETE</button>
+                <p>Title: {item.EpisodeTitle}</p>
+                <p>Episode: {item.EpisodeNumber}</p>
+                <p>Added: {AddedDate.toLocaleDateString('en-GB', { hour: '2-digit', minute: '2-digit' })}</p>
+                <div className="favButtons">
+                    <button onClick={() => handleDelete(item.EpisodeTitle)}>DELETE</button>
+                    <button id={item.EpisodeFile} title={item.EpisodeTitle} onClick={props.HandleAudioPlay}>Play</button>
+                </div>
             </div>
         )
     })
 
     return (
-        <div>
-            {
-            state === 'loading' ? <div>{"LOADING..."}</div> : favouriteElements
-            }
-        </div>
+        <>
+            <div className='SortNav'>
+                <button onClick={sortByAscending}>A-Z</button>
+                <button onClick={sortByDescending}>Z-A</button>
+                <button onClick={sortByLatest}>Latest</button>
+                <button onClick={sortByOldest}>Oldest</button>
+            </div>
+            <div className="favouriteSection">
+                <h1 className="favTitle">FAVOURITES</h1>
+                {
+                    state === 'loading' ? <div>{"LOADING..."}</div> :
+                        favouriteData.length === 0 ? <h1>No Favourites Available</h1> :
+                            <div className="favourites">{favouriteElements}</div>
+                }
+
+            </div>
+        </>
     )
 }
